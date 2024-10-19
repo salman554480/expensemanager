5f995cfecdb52b89375ca5a1f31e3ebdb962ee56
salman554480 <sa307485@gmail.com>
2024-10-19 15:56:37 -0700

    Update Sidebar

--- parts/sidebar.php
+++ parts/sidebar.php
@@ -14,15 +14,14 @@
         <li><a class="app-menu__item" href="index.php"><i class="app-menu__icon bi bi-speedometer"></i><span
                     class="app-menu__label">Dashboard</span></a></li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                    class="app-menu__icon bi bi-laptop"></i><span class="app-menu__label">UI Elements</span><i
+                    class="app-menu__icon bi bi-laptop"></i><span class="app-menu__label">Category</span><i
                     class="treeview-indicator bi bi-chevron-right"></i></a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon bi bi-circle-fill"></i>
-                        Bootstrap Elements</a></li>
-                <li><a class="treeview-item" href="https://icons.getbootstrap.com/" target="_blank" rel="noopener"><i
-                            class="icon bi bi-circle-fill"></i> Font Icons</a></li>
-                <li><a class="treeview-item" href="ui-cards.html"><i class="icon bi bi-circle-fill"></i> Cards</a></li>
-                <li><a class="treeview-item" href="widgets.html"><i class="icon bi bi-circle-fill"></i> Widgets</a></li>
+                <li><a class="treeview-item" href="category_view.php"><i class="icon bi bi-circle-fill"></i> View
+                        Categories</a>
+                </li>
+                <li><a class="treeview-item" href="category_add.php"><i class="icon bi bi-circle-fill"></i> Add
+                        Category</a></li>
             </ul>
         </li>
 
